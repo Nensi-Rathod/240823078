79b1db74a50a99480441d24c430d8996a11b1b17
Nensi-Rathod <nensirathod2003@gmail.com>
2025-07-14 11:27:12 +0530

    Assignment_1

---
+++ Assignment_1.js
@@ -0,0 +1,20 @@
+// 1. Check Even or Odd
+
+// function checkEvenOdd(number){
+//     if (number % 2 === 0){
+//         console.log("Even");
+
+//     }else{
+//         console.log("odd");
+//     }
+// }
+// const input=7;
+// checkEvenOdd(input)
+
+//2. Find the Maximum of Two Numbers
+
+// const num1 = 5;
+// const num2 = 9;
+
+// console.log(Math.max(num1, num2));
+
